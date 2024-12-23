030337c61695536eb21a5fdee7a534d5f0eb2538
PJE\EzYong-Laptop <ckgirls123@naver.com>
2024-12-23 19:05:25 +0900

    수집아이템용 클래스 추가중

---
+++ Source/CTTPractice/CTTCollectibleItem.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CTTCollectibleItem.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+ACTTCollectibleItem::ACTTCollectibleItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	SetRootComponent(CapsuleComponent);
+}
+
+// Called when the game starts or when spawned
+void ACTTCollectibleItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACTTCollectibleItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/CTTPractice/Managers/CTTSoundManager.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CTTSoundManager.generated.h"
+
+/**
+ * 
+ */
+
+class UAudioComponent;
+
+UCLASS(Blueprintable)
+class CTTPRACTICE_API UCTTSoundManager : public UObject
+{
+	GENERATED_BODY()
+	
+
+public:
+	void Initialize();
+
+private:
+};
